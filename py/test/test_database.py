6cd90be6a037c11362c8bca28717271a85b234a9
carrvo <carrvo@gmail.com>
2022-07-17 19:41:05 -0600

    database filter API

--- py/test/test_database.py
+++ py/test/test_database.py
@@ -51,3 +51,33 @@ class DatabaseTests(unittest.TestCase):
             self.assertEqual(tags[1]['path'], 'food / groceries')
             db.remove_tag(items[0], 'food / groceries')
             self.assertEqual(tuple(db.tags)[0]['item'], items[1]['id'])
+
+    def test_filters(self):
+        """
+        Tests filtering logic.
+        """
+        with DatabaseLayer(':memory:') as db:
+            db.insert('business', b'test', 12.34, (
+                {'name':'test float', 'price':1.34},
+                {'name':'test integer', 'price':10},
+                {'name':'test too many digits', 'price':1.00003}
+            ))
+            receipts = tuple(db.receipts)
+            items = tuple(db.items)
+            db.add_tag(items[0], 'food / groceries')
+            db.add_tag(items[1], 'food / groceries')
+            tags = tuple(db.tags)
+            filter = BusinessFilter(db)
+            self.assertEqual(filter('business'), receipts[0])
+            filter = ItemFilter(db)
+            self.assertEqual(filter('test integer'), items[1])
+            filter = PriceFilter(db, limit=True)
+            self.assertEqual(len(filter(1.1)), 2)
+            filter = PriceFilter(db, limit=False)
+            self.assertEqual(len(filter(5.0)), 2)
+            filter = ReceiptFilter(db)
+            self.assertEqual(filter(receipts[0]), items)
+            filter = TagFilter(db)
+            self.assertEqual(filter('food / groceries'), items[0:1])
+            filter = ItemTags(db)
+            self.assertEqual(tuple(filter(items[1]))[0], 'food / groceries')
